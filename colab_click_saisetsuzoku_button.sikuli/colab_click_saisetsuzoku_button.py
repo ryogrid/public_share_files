13d744b99814ebd3bf98ffcaebf3be4962e28f83
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-04-27 20:30:33 +0900

    add sikuli script for google colaboratory.

---
+++ colab_click_saisetsuzoku_button.sikuli/colab_click_saisetsuzoku_button.py
@@ -0,0 +1,11 @@
+import time
+setFindFailedResponse(SKIP)
+while True:
+    time.sleep(1200)
+    if exists("082965c4bd323467086f0552fab372d0.png"):
+        click("082965c4bd323467086f0552fab372d0.png")
+    if exists("saisetsuzoku_kirinuki.jpg"):
+        click("saisetsuzoku_kirinuki.jpg")
+
+
+        
